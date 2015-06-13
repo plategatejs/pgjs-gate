929cf8ab198fe9e895cdd8a2a69844652d58f49d
MW <mateuszwag@gmail.com>
2015-06-13 13:21:45 +0200

    Fix idendations.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     config = require('config'),
-	sys = require('sys'),
-	exec = require('child_process').exec;
+    sys = require('sys'),
+    exec = require('child_process').exec;
 
 var properties = {
     openGateScript: config.get('scripts.openGate'),
@@ -14,14 +14,14 @@ function pipeOut(error, stdout, stderr) { sys.puts(stdout) }
 var app = express();
 
 app.get('/openGate/', function (req, res) {
-	exec(properties.openGateScript, pipeOut);
+    exec(properties.openGateScript, pipeOut);
     res.writeHead(200, {
         'content-type': 'text/plain',
         'cache-control': 'private, no-cache, no-store, must-revalidate',
         'expires': '0',
         'pragma': 'no-cache'
     });
-	res.end('Opening!');
+    res.end('Opening!');
 });
 
 app.listen(properties.port);
